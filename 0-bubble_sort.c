02ae5b1b682df58479efbb8224eb982c99af64bb
Bantamlak <neoluka315@gmail.com>
2023-01-23 09:25:14 +0000

    considering if array is NULL

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,6 +10,9 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t temp;
 	size_t i, j;
+	
+	if (array == NULL)
+		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
@@ -20,7 +23,6 @@ void bubble_sort(int *array, size_t size)
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
-
 				print_array(array, size);
 			}
 		}
